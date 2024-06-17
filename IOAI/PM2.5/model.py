ae0408f16ba57145a84315790194504102013308
pvh261 <pvh2617@gmail.com>
2024-06-18 00:25:47 +0700

    commit

--- IOAI/PM2.5/model.py
+++ IOAI/PM2.5/model.py
@@ -8,6 +8,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.metrics import mean_absolute_percentage_error
 import pickle
+from datetime import datetime
 
 PATH = os.path.join("IOAI", "PM2.5")
 
@@ -20,10 +21,14 @@ def prepare():
    train_set, test_set = load_data()
    train_set.dropna(subset=["PM2.5"], inplace=True)
 
-   train_label = train_set[["PM2.5"]]
+   train_set = split_datetime(train_set)
+   test_set = split_datetime(test_set)
 
-   train_set.drop(["Datetime", "PM2.5"], axis=1, inplace=True)
-   test_set.drop(["Datetime"], axis=1, inplace=True)
+   print(train_set)
+   print(test_set)
+
+   train_label = train_set[["PM2.5"]]
+   train_set.drop(["PM2.5"], axis=1, inplace=True)
 
    num_pipeline = Pipeline([
       ("imputer", SimpleImputer(strategy="median")),
@@ -45,13 +50,28 @@ def prepare():
    print("Done")
    zeros = np.zeros((14259, 87))
    test_set_prepared = np.hstack((test_set_prepared, zeros))
-   print("Done")
    
-   model = Lasso()
-   model.fit(train_set_prepared, train_label)
-   with open('IOAI/PM2.5/ml_model.pkl', 'wb') as f:
-      pickle.dump(model, f)
+   # model = Lasso()
+   # model.fit(train_set_prepared, train_label)
+   # with open('IOAI/PM2.5/ml_model.pkl', 'wb') as f:
+   #    pickle.dump(model, f)
 
-   print(mean_absolute_percentage_error(train_label, model.predict(train_set_prepared)))
+   # print(mean_absolute_percentage_error(train_label, model.predict(train_set_prepared)))
+
+def split_datetime(dataframe):
+   timeset = dataframe.loc[:, 'Datetime']
+   date = []
+   month = []
+   hour = []
+   for time in timeset:
+      object = datetime.strptime(time, '%Y-%m-%d  %H:%M:%S')
+      date.append(object.day)
+      month.append(object.month)
+      hour.append(object.hour)
+   dataframe.drop(['Datetime'], axis=1, inplace=True)
+   dataframe['Month'] = month
+   dataframe['Date'] = date
+   dataframe['Hour'] = hour
+   return dataframe
 
 prepare()
--- tools.py
+++ tools.py
@@ -1,8 +1,4 @@
-import PIL.Image
-import cv2
-
-image = cv2.imread("0.jpg")
-print(image[0])
-image = cv2.resize(image, (227, 227))
-print(image[0])
-cv2.imwrite("1.jpg", image)
+from ultralytics import YOLO
+model = YOLO("yolov8n.pt")  # load an official model
+results = model.predict("0.jpg")
+print(results.names[int(box.cls[0])])
